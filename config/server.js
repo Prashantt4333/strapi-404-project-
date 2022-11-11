0eb4244d96f15aed0061b4543f1fde592a896b28
Prashant tak <prashantt4333@gmail.com>
2022-11-11 14:07:26 +0530

    update file

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['Ly2hIKjLP+i+x+1VkGvJag==', 'hNnvLlux8LhReByNbgiybA==','ErRHM/KOYSf1cOsgkr/kJA==','JZh6GOj6gdfK5dnJ49a+gg==']),
   },
 });
