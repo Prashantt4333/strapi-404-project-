321c25826b1e6725371c0b4dc7099101e51d37a6
Prashant tak <prashantt4333@gmail.com>
2022-11-11 17:23:46 +0530

    new

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,7 +2,7 @@
 
 module.exports = ({ env }) => ({
     url: env('MY_HEROKU_URL'),
-    proxy: true,
+    proxy: true, 
     app: {
       keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
     },
